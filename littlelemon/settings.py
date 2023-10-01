0b4d8c3925df5f55bd55c1933659514cd3280aec
marvi2424 <marvi.vani24@gmail.com>
2023-10-01 17:46:12 +0200

    small fixes

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -38,11 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # The settings for app updated for the Graded assessment
     "restaurant",
-    # "rest_framework",
-    # "rest_framework.authtoken",
-    # "djoser",
 ]
 
 MIDDLEWARE = [
@@ -139,25 +135,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-REST_FRAMEWORK = {
-    "DEFAULT_FILTER_BACKENDS": [
-        "rest_framework.filters.OrderingFilter",
-        "rest_framework.filters.SearchFilter",
-    ],
-    "DEFAULT_RENDERER_CLASSES": [
-        "rest_framework.renderers.BrowsableAPIRenderer",
-        "rest_framework.renderers.JSONRenderer",
-    ],
-    # Add code to assign default authentication classes
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
-    ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 3,
-}
-
-DJOSER = {
-    "USER_ID_FIELD": "username",
-}
